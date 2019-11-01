bbff5d90584ab33bd7b7480f7daec15c5d490ee0
Thanh-Minh <41420911+Thanh-Minh@users.noreply.github.com>
2019-11-01 14:50:08 +0700

    update

--- src/theme/pages/Chothue.js
+++ src/theme/pages/Chothue.js
@@ -1,27 +1,93 @@
 //This is an example code for NavigationDrawer//
 import React, {Component} from 'react';
 //import react in our code.
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Image,TouchableOpacity} from 'react-native';
 // import all basic components
 
 export default class Chothue extends Component {
-  //Chothue Component
-  render() {
-    return (
-      <View style={styles.MainContainer}>
+    //Chothue Component
+    render() {
+        return (
+            <View style={styles.MainContainer}>
+                <View style={styles.Item1}>
+                    <View>
+                        <Image
+                            style={styles.Image}
+                            source={{uri: 'https://redata.vn//public/home/img/default.gif'}}
+                        />
+                    </View>
+                    <View style={{marginLeft:6}}>
+                    <View >
+                        <Text numberOfLines={1} style={styles.Title}>CHO THUÊ CĂN HỘ CHUNG CƯ TÒA NHÀ TRUNG YÊN PLAZA SỐ 1 TRUNG HÒA</Text>
+                    </View>
+                    <View style={styles.content}>
+                        <Image
+                            style={{width: 14, height: 14,marginTop: 4}}
+                            source={require('../../asset/images/icon_location.png')}/>
+                        <View  style={styles.TextType}>
+                            <Text>Vị trí:</Text>
+                            <Text>Trung Hoà, Cầu Giấy, Hà Nội</Text>
+                        </View>
+                    </View>
+                    <View style={styles.content}>
+                        <Image
+                            style={{width: 14, height: 14,marginTop: 4}}
+                            source={require('../../asset/images/icon_vuong.png')}/>
+                        <View  style={styles.TextType}>
+                            <Text>Diện tích:</Text>
+                            <Text>110 m2</Text>
+                        </View>
+                    </View>
+                        <View  style={styles.content}>
+                            <View
+                                  style={styles.TextType}>
+                                <Text>Tài chính:</Text>
+                                <Text>15 Triệu/tháng</Text>
+                            </View>
+                        </View>
+                        <TouchableOpacity>
+                            <Text>Nhận tư vấn</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
 
-        <Text style={{fontSize: 23}}> Screen 3 </Text>
-      </View>
-    );
-  }
+            </View>
+
+        );
+    }
 }
-
 const styles = StyleSheet.create({
-  MainContainer: {
-    flex: 1,
-    paddingTop: 20,
-    alignItems: 'center',
-    marginTop: 50,
-    justifyContent: 'center',
-  },
+    MainContainer: {
+        flex: 1,
+
+    },
+    Image: {
+        width: '100%',
+        height: 200,
+        alignItems: 'center',
+        marginTop: 6,
+        marginBottom: 6,
+
+    },
+   Item1:{
+        margin:6,
+       borderWidth:1,
+       borderColor: '#ff0000',
+
+
+    },
+    Title:{
+        color:'#ff0000',
+
+    },
+    content:{
+        flexDirection:'row',
+
+    },
+    TextType:{flexDirection: 'row'}
 });
+
+
+
+
+
--- src/theme/pages/Naptien.js
+++ src/theme/pages/Naptien.js
@@ -9,7 +9,23 @@ export default class Naptien extends Component {
     render() {
         return (
             <View style={styles.MainContainer}>
-                <Text style={{fontSize: 23}}> Screen 3 </Text>
+                <Text>Hướng dẫn nạp tiền</Text>
+                <Text>Quý khách có thể chuyển khoản cho chúng tôi theo các tài khoản bên dưới, sau khi chuyển các bạn
+                    vui lòng liên hệ 0989.625.304 - 02438.9919.866 để xác nhận, chúng tôi sẽ tiến hành nạp tiền cho bạn
+                    ngay khi nhận được tiền và thông báo tới quý khách:</Text>
+                <Text>THÔNG TIN TÀI KHOẢN :</Text>
+                <View>
+                <Text>Số tài khoản : </Text>
+                    <Text>21510001866237</Text>
+                </View>
+                <View>
+                    <Text>Chủ tài khoản : </Text>
+                    <Text>ĐẶNG THỊ LAN ANH</Text>
+                </View>
+
+                    <Text>Ngân hàng TMCP Đầu tư và phát triển (BIDV) Chi nhánh Thăng Long </Text>
+                    <Text>Nội dung: Nạp tiền cho (Email hoặc Phone) </Text>
+
             </View>
         );
     }
@@ -18,9 +34,6 @@ export default class Naptien extends Component {
 const styles = StyleSheet.create({
     MainContainer: {
         flex: 1,
-        paddingTop: 20,
-        alignItems: 'center',
-        marginTop: 50,
-        justifyContent: 'center',
+
     },
 });
